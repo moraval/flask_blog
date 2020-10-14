2d4f4537306bfd6195ffaf3fcd5691d3b1850fc8
moraval <alena.moravova@gmail.com>
2020-10-14 15:15:44 +0200

    Working login and registration forms

--- flask_blog.py
+++ flask_blog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9975046c1e9cac3f0fc17636d0b29866'
 
 posts = [
     {
@@ -27,6 +29,24 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Hi {form.email.data}, you're logged in!", 'success')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
